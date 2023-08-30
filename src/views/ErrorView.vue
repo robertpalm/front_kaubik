f271876b16bd547fd2595a40f43a99d54ef2d3dd
robertpalm <robert.rp.palm@gmail.com>
2023-08-30 14:14:49 +0300

    ErrorView

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Midagi läks valesti</h1>
+  <font-awesome-icon :icon="['far', 'face-sad-cry']" size="2xl" />
+</template>
+
+<script>
+export default {
+  name: "ErrorView"
+}
+</script>
+
+
+<style scoped>
+
+</style>
